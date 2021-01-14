61e5add92f8b03dd67f81d494c78fc625f094a7e
justin-mavity <justinmavity1021@gmail.com>
2021-01-13 18:58:54 -0600

    finished

--- src/index.js
+++ src/index.js
@@ -1,27 +1,30 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
-
+import useDarkMode from "./hooks/useDarkMode";
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
 
 import "./styles.scss";
-
+const initialValue = {
+  darkMode: false,
+};
 const App = () => {
   const [coinData, setCoinData] = useState([]);
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, toggleMode] = useDarkMode(initialValue);
 
   useEffect(() => {
     axios
       .get(
         "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
       )
-      .then(res => setCoinData(res.data))
-      .catch(err => console.log(err));
+      .then((res) => setCoinData(res.data))
+      .catch((err) => console.log(err));
   }, []);
+
   return (
-    <div className={darkMode ? "dark-mode App" : "App"}>
-      <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
+    <div className={darkMode ? true : false}>
+      <Navbar darkMode={darkMode} setDarkMode={toggleMode} />
       <Charts coinData={coinData} />
     </div>
   );
